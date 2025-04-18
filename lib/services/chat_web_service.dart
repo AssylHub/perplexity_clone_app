c1a15cec0483bddba7ada0a335c37bee596ed248
Assyl <assylzfg@gmail.com>
2025-04-18 15:04:15 +0500

    Connected

---
+++ lib/services/chat_web_service.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+
+import 'package:web_socket_client/web_socket_client.dart';
+
+class ChatWebService {
+  static final _instance = ChatWebService._internal();
+  WebSocket? _socket;
+
+  factory ChatWebService() =>_instance;
+
+  ChatWebService._internal();
+
+  void connect() {
+    _socket = WebSocket(Uri.parse("ws://localhost:8000/ws/chat"));
+
+    _socket!.messages.listen((message) {
+      final data = jsonDecode(message);
+      print(data["type"]);
+    });
+  }
+
+  void chat(String query) {
+    print(query);
+    print(_socket);
+    _socket!.send({"query": query});
+  }
+}
