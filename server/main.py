b3ca7f7bf153a13120452fdf4c40d6e194526569
Assyl <assylzfg@gmail.com>
2025-04-18 11:47:53 +0500

    Generate response using LLM Service

--- server/main.py
+++ server/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 
 from pydantic_models.chat_body import ChatBody
+from services.llm_service import LLMService
 from services.sort_source_service import SortSourceService
 from services.search_service import SearchService
 
@@ -8,6 +9,7 @@ app = FastAPI()
 
 search_service = SearchService()
 sort_source_service = SortSourceService()
+llm_service = LLMService()
 
 # chat
 
@@ -16,9 +18,12 @@ sort_source_service = SortSourceService()
 def chat_endpoint(body: ChatBody):
     # search the web and find appropriate sources
     search_results = search_service.web_search(body.query)
-    # print(search_results)
+
     # sort the sources
     sorted_results = sort_source_service.sort_sources(body.query, search_results)
     print(sorted_results)
+
     # generate the response using LLM
-    return body.query
+    response = llm_service.generate_response(body.query, sorted_results)
+
+    return response
